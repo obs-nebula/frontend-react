ebc1d5119911e1b8107ca9feae3540fa3c28fa6d
pnaik <pnaik@redhat.com>
2022-12-05 11:49:37 +0530

    chore: adding jaeger

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import TraceProvider from './tracing';
-alert("hiiiii")
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
