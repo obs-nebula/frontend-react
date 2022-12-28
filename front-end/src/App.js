0dba09006d361d4de8a63917f1ee7b5f49855865
Helio Frota <00hf11@gmail.com>
2022-12-28 08:07:39 -0300

    chore: adding some license headers

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { useState } from 'react';
 import './App.css';
 
@@ -7,21 +23,18 @@ function App () {
     const response = await fetch('http://localhost:5000/express_backend');
     const body = await response.json();
 
-    
     if (response.status !== 200) {
       throw Error(body.message)
     }
+
     useValueFunction(body.express)
   }
+
   return (
     <div className='App'>
-
       <h1>This is a simple react project</h1>
       <button id='btn' onClick={MyExpressFunction}>Click here please</button>
-
       <p className='result'>{useValue}</p>
-
-
     </div>
   );
 }
