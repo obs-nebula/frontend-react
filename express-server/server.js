3ee65a8f5a598ad85d2cb06d6e0fda114c121c9f
pnaik <pnaik@redhat.com>
2023-01-17 14:59:19 +0530

    chore: changing the structure of frontend

--- express-server/server.js
+++ express-server/server.js
@@ -25,6 +25,8 @@ app.use(cors());
 
 app.get('/express_backend', (_, res) => {
   console.log('button clicked');
-  res.send({ express: 'Welcome to the project' });
+  res.send({
+    express: 'An employee information form contains important details about your employees. Use it to keep track of personal information, duration of employment and other essentials, such as emergency contact information. An employee’s form is also useful if you receive a reference request, need to mail documents to an employee or want to know how long someone has been with your companyEmployee information forms are important in many events. If there’s an urgent need to reach out to them, an employee’s information form will give you more ways to reach them. Furthermore, you can use these forms for emergency contact information if the worker is seriously injured.'
+  });
 });
 
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,10 +16,13 @@
 
 import { useState } from 'react';
 import './App.css';
-
+import InfiniteScroll from "./InfiniteScroll"
 function App () {
   const [useValue, useValueFunction] = useState();
+  const [toggle, setToggle] = useState(false)
   async function MyExpressFunction () {
+
+
     const response = await fetch('http://localhost:5000/express_backend');
     const body = await response.json();
 
@@ -28,13 +31,24 @@ function App () {
     }
 
     useValueFunction(body.express)
-  }
 
+  }
   return (
     <div className='App'>
-      <h1>This is a simple react project</h1>
-      <button id='btn' onClick={MyExpressFunction}>Click here please</button>
-      <p className='result'>{useValue}</p>
+      <div style={{ margin: "2% 30%", textAlign: "left" }}>
+        <h1 >Details of Employees </h1>
+        <p>
+          An employee information form ensures that you have current details on employees, including contact details and emergency contact information.
+          Collecting and updating these forms periodically will allow you to keep track of your employees and reference details when you need them. Learn what to include on an employee information form and how to create one for your business.
+        </p>
+        <button id='btn' style={{ padding: "0.5% 3%", backgroundColor: "indianred" }} onClick={() => {
+          setToggle(!toggle);
+          MyExpressFunction();
+        }}>Learn more </button>
+
+        {toggle ? <p className='result'>{useValue}</p> : ""}
+      </div>
+      <InfiniteScroll />
     </div>
   );
 }
