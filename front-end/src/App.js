bc2ac2cfa3ae775c73fd83100ae8fad2c8c5223a
pnaik <pnaik@redhat.com>
2023-02-10 12:49:25 +0530

    chore: added eslint

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -16,10 +16,10 @@
 
 import { useState } from 'react';
 import './App.css';
-import InfiniteScroll from "./InfiniteScroll"
+import InfiniteScroll from './InfiniteScroll';
 function App () {
   const [useValue, useValueFunction] = useState();
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
   async function MyExpressFunction () {
 
 
@@ -27,26 +27,26 @@ function App () {
     const body = await response.json();
 
     if (response.status !== 200) {
-      throw Error(body.message)
+      throw Error(body.message);
     }
 
-    useValueFunction(body.express)
+    useValueFunction(body.express);
 
   }
   return (
     <div className='App'>
-      <div style={{ margin: "2% 30%", textAlign: "left" }}>
+      <div style={{ margin: '2% 30%', textAlign: 'left' }}>
         <h1 >Details of Employees </h1>
         <p>
           An employee information form ensures that you have current details on employees, including contact details and emergency contact information.
           Collecting and updating these forms periodically will allow you to keep track of your employees and reference details when you need them. Learn what to include on an employee information form and how to create one for your business.
         </p>
-        <button id='btn' style={{ padding: "0.5% 3%", backgroundColor: "indianred" }} onClick={() => {
+        <button id='btn' style={{ padding: '0.5% 3%', backgroundColor: 'indianred' }} onClick={() => {
           setToggle(!toggle);
           MyExpressFunction();
         }}>Learn more </button>
 
-        {toggle ? <p className='result'>{useValue}</p> : ""}
+        {toggle ? <p className='result'>{useValue}</p> : ''}
       </div>
       <InfiniteScroll />
     </div>
